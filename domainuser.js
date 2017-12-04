e829f29db9c38d5f1e9384e959f6e3856dcd9cb4
miquel <miquel.cabot@gmail.com>
2017-12-04 21:56:46 +0100

    Implementació funció normalizedname

--- domainuser.js
+++ domainuser.js
@@ -11,22 +11,23 @@ function removeaccents(str) {
    
 function normalizedname(name) {
   var tokens = removeaccents(name.toLowerCase()).split(" ");
+  var names = [];
+  // Words with compound names and surnames
+  especial_tokens = ['da', 'de', 'di', 'do', 'del', 'la', 'las', 'le', 'los', 
+    'mac', 'mc', 'van', 'von', 'y', 'i', 'san', 'santa','al','el'];
   
-  return name;
-  
-  /*
-      tokens = remove_accents(name.lower()).split()
-    names = []
-    # Words with compound names and surnames
-    especial_tokens = ['da', 'de', 'di', 'do', 'del', 'la', 'las', 'le', 'los', 'mac', 'mc', 'van', 'von', 'y', 'i', 'san', 'santa','al','el']
-    for token in tokens: 
-        if not token in especial_tokens:
-            names.append(token)
-    if len(names)>=1:       # If name exists (with name or surname)
-        return (names[0])
-    else:                   # If name not exists (without name or surname)
-        return "_"
-  */
+  for (var i = 0; i < tokens.length; i++) {
+    token = tokens[i];
+    if (especial_tokens.indexOf(token)<0) {  // If token not in especial_tokens
+      names.push(token);
+    }
+  }
+
+  if (names.length>=1) { // If name exists (with name or surname)
+    return names[0];
+  } else {               // If name not exists (without name or surname)
+    return "_";
+  }
 }
 
 function DomainUser(domain, id, name, surname, surname1, surname2, domainemail, suspended, teacher, groups) {
@@ -39,7 +40,7 @@ function DomainUser(domain, id, name, surname, surname1, surname2, domainemail, 
   this.domainemail = domainemail;
   this.suspended = suspended;
   this.teacher = teacher;
-  this.groups = ["groups","A"];
+  this.groups = groups;
 
   this.email = function() {
     if (this.domainemail) {
@@ -64,7 +65,7 @@ function DomainUser(domain, id, name, surname, surname1, surname2, domainemail, 
 // toString override added to prototype of DomainUser class
 DomainUser.prototype.toString = function()
 {
-    return this.teacher?"TEACHER: ":"STUDENT: "+this.surname+", "+this.name+" ("+this.email()+") ["+this.groups+"]";
+    return (this.teacher?"TEACHER: ":"STUDENT: ")+this.surname+", "+this.name+" ("+this.email()+") ["+this.groups+"]";
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var domainuser = require("./domainuser");
 
-var a = new domainuser.DomainUser("iesemilidarder.com", 1, "Pep", "Guardiola Sanç", "Guardiola", "Sanç", "pguardiola@iesemilidarder.com");
+var a = new domainuser.DomainUser("iesemilidarder.com", 1, "Pep", 
+    "Guardiola Sanç", "Guardiola", "Sanç", "", false, false);
 
 console.log(a);
 console.log(a.toString());
